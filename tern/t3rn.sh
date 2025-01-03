749bd43afcc8d67fe52016eaefae820e3090eecd
Darksun <43103129+xhinis@users.noreply.github.com>
2025-01-03 11:30:48 +0300

    Update t3rn.sh

--- tern/t3rn.sh
+++ tern/t3rn.sh
@@ -1,4 +1,3 @@
-
 update_node() {
   delete_node
 
@@ -46,31 +45,4 @@ update_node() {
 
 download_node() {
   if [ -d "$HOME/executor" ] || screen -list | grep -q "\.t3rnnode"; then
-    echo 'The executor folder or the t3rnnode session already exists. Installation is not possible. Choose to delete the node or exit the script.'
-    return
-  fi
-
-  echo 'Starting node installation...'
-
-  read -p "Enter your private key: " PRIVATE_KEY_LOCAL
-
-  sudo apt update -y && sudo apt upgrade -y
-  sudo apt-get install make screen build-essential software-properties-common curl git nano jq -y
-
-  cd $HOME
-
-  sudo wget https://github.com/t3rn/executor-release/releases/download/v0.29.0/executor-linux-v0.29.0.tar.gz -O executor-linux.tar.gz
-  sudo tar -xzvf executor-linux.tar.gz
-  sudo rm -rf executor-linux.tar.gz
-  cd executor
-
-  export NODE_ENV="testnet"
-  export LOG_LEVEL="debug"
-  export LOG_PRETTY="false"
-  export EXECUTOR_PROCESS_ORDERS="true"
-  export EXECUTOR_PROCESS_CLAIMS="true"
-  export PRIVATE_KEY_LOCAL="$PRIVATE_KEY_LOCAL"
-  export ENABLED_NETWORKS="arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn"
-  export RPC_ENDPOINTS_BSSP="https://base-sepolia-rpc.publicnode.com"
-  export RPC_ENDPOINTS_L1RN='https://brn.rpc.caldera.xyz/'
-  export EXECUTOR_MAX_L3
+    echo 'The executor folder or the t3rnnode session already exists. Installation is not possible. Choose to delete the node or exit the
